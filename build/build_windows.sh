cc6cef26a747827dfe49357268cefc9228bf5e57
Benjamin Nicholls <nicholls.ben@gmail.com>
2025-02-21 14:39:34 -0500

    update gitignore, build folder for building the builds while building
    - also added build scripts so people can build easier

---
+++ build/build_windows.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "BUILDING FOR WINDOWS IN DEBUG MODE"
+env CGO_ENABLED="1" CC="/usr/bin/x86_64-w64-mingw32-gcc" GOOS="windows" CGO_LDFLAGS="-lmingw32 -lSDL2" CGO_CFLAGS="-D_REENTRANT" go build -C "../game" -tags debug
+
+if [ $? -eq "0" ]
+then 
+echo "BUILD COMPLETE"
+mv ../game/game.exe tytris.exe
+./tytris.exe
+else echo "POBLEMS, BUILD DID NOT GO WELL"
+fi
---
+++ build/build_windows_release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "BUILDING RELEASE FOR WINDOWS"
+env CGO_ENABLED="1" CC="/usr/bin/x86_64-w64-mingw32-gcc" GOOS="windows" CGO_LDFLAGS="-lmingw32 -lSDL2" CGO_CFLAGS="-D_REENTRANT" go build -C ../game/ -ldflags  "-H=windowsgui"
+
+if [ $? -eq "0" ]
+then 
+echo "BUILD COMPLETE"
+mv ../game/game.exe tytris_release.exe
+else echo "POBLEMS, BUILD DID NOT GO WELL"
+fi
